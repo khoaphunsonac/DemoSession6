cf28f1cf43a275a9de951a498f28e941e5c51263
khoaphunsonac <khoa.trandang020704@hcmut.edu.vn>
2024-10-09 10:03:28 +0700

    New update

--- demo6.ts
+++ demo6.ts
@@ -11,21 +11,62 @@ let products = [
     'p10,speaker,8.3,12'
 ];
 
-Number
-function listObject(obj) {
-    let str = obj.split(",");
-    console.log("\tID: " + str[0]);
-    console.log("\tName: " + str[1]);
-    console.log("\tPrice: " + str[2]);
-    console.log("\tQuantity: " + str[3]);
-    console.log("\tTotal: " + (Number(str[2]) * Number(str[3])).toFixed(2));
-}
-
 function listProduct(product) {
     for (let i = 1; i <= product.length; i++) {
         console.log(`Sản phẩm thứ ${i}:`);
-        listObject(product[i-1]);
+        let str = product[i - 1].split(",");
+        console.log("\tID: " + str[0]);
+        console.log("\tName: " + str[1]);
+        console.log("\tPrice: " + str[2]);
+        console.log("\tQuantity: " + str[3]);
+        console.log("\tTotal: " + (Number(str[2]) * Number(str[3])).toFixed(2));
     }
 }
 
-listProduct(products);
+//listProduct(products);
+/**
+ * 1. Xây dựng hiển thị thông tin của 1 sản phẩm dựa trên chuỗi được cung cấp 
+ * 2. Xây dựng hàm liệt kê các sản phẩm có tên chứa 1 từ khóa
+ * 3. Kiểm tra 1 mã sản phẩm có tồn tại trong danh sách sản phẩm hay không
+ * 4. Liệt kê các sản phẩm có giá nằm trong khoảng từ min đến max
+ */
+
+function listInforOfProduct(product) {
+    let str = product.split(",");
+    console.log("ID: " + str[0]);
+    console.log("Name: " + str[1]);
+    console.log("Price: " + str[2]);
+    console.log("Quantity: " + str[3]);
+}
+
+function listProductWithKey(products, key) {
+    for (let i = 0; i < products.length; i++) {
+        let s = products[i].split(",");
+        if (s[1] == key) {
+            listInforOfProduct(products[i]);
+            console.log("-----------------------");
+        };
+    }
+}
+
+function checkProductWithKey(products, key) {
+    for (let i = 0; i < products.length; i++) {
+        let s = products[i].split(",");
+        if (s[1] == key) return true;
+    }
+    return false;
+}
+
+function listWithPrice(products, min, max) {
+    for (let i = 0; i < products.length; i++) {
+        let s = products[i].split(",");
+        if (s[3] >= min && s[3] <= max) {
+            listInforOfProduct(products[i]);
+            console.log("-----------------------");
+        };
+    }
+}
+// Test
+listProductWithKey(products, "tablet");
+console.log(checkProductWithKey(products, "laptop"));
+listWithPrice(products, 5, 12);
